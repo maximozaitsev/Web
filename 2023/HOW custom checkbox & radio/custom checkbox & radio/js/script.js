f2d514f1ccf42e996c46d1c1fdf34c990ba4f832
maximozaitsev <maximozaitsev@gmail.com>
2023-03-19 12:20:24 +0300

    radio

--- 2023/HOW custom checkbox & radio/custom checkbox & radio/js/script.js
+++ 2023/HOW custom checkbox & radio/custom checkbox & radio/js/script.js
@@ -13,4 +13,24 @@ $(document).ready(function () {
     }
     $(this).toggleClass("active");
   });
+
+  // RADIO
+  $.each($(".radiobutton__item"), function (index, val) {
+    if ($(this).find("input").prop("checked") == true) {
+      $(this).addClass("active");
+    }
+  });
+  $(document).on("click", ".radiobutton__item", function (event) {
+    $(this)
+      .parents(".radiobuttons")
+      .find(".radiobutton__item")
+      .removeClass("active");
+    $(this)
+      .parents(".radiobuttons")
+      .find(".radiobutton__item input")
+      .prop("checked", false);
+    $(this).toggleClass("active");
+    $(this).find("input").prop("checked", true);
+    return false;
+  });
 });
