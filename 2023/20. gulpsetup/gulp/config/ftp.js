993b0ec0aceccb6d0ea24722a4c06db85e9f29f0
maximozaitsev <maximozaitsev@gmail.com>
2023-03-22 14:18:01 +0300

    gulpsetup

---
+++ 2023/20. gulpsetup/gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+  host: "file:///D:/%D0%B4%D0%BE%D0%BA%D1%83%D0%BC%D0%B5%D0%BD%D1%82%D1%8B/IT/GitHub%20repositories/Web/2023/setgulp/test/setgulp", // Адрес FTP сервера
+  user: "Max", // Имя пользователя
+  password: "2104", // Пароль
+  parallel: 5, // Кол-во одновременных потоков
+};
