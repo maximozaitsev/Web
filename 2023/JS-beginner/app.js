7b6ee3b6112c7ba4c8ef4ef6f755bfb08d71ac40
maximozaitsev <maximozaitsev@gmail.com>
2023-04-05 21:57:54 +0300

    js-data-types

--- 2023/JS-beginner/app.js
+++ 2023/JS-beginner/app.js
@@ -1,7 +1,33 @@
 // 1 Переменные
 // variable
-const firstName = "Maxim";
-const lastName = "Zaitsev";
-const age = 22;
+// const firstName = "Maxim";
+// const lastName = "Zaitsev";
+// const age = 22;
 
-console.log("имя человека: " + firstName);
+// 2 Мутирование
+// console.log("имя человека: " + firstName + ", а возраст: " + age);
+// alert("имя человека: " + firstName + ", а возраст: " + age);
+
+// const lastName = prompt("Введите фамилию");
+// alert(firstName + " " + lastName);
+
+// 3 Операторы
+// const a = 10;
+// const b = 5;
+
+// let c = 32;
+// c = c + a
+// c += a;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(c);
+
+// 4 Типы данных
+const isProgrammer = true;
+const firstName = "Max";
+const age = 22;
+console.log(typeof isProgrammer);
+console.log(typeof firstName);
+console.log(typeof age);
