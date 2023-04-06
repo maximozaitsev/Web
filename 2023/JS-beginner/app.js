98b5107d51e44cb804e0acb55c73f7090bb6048b
maximozaitsev <maximozaitsev@gmail.com>
2023-04-06 21:31:38 +0300

    js-arrays

--- 2023/JS-beginner/app.js
+++ 2023/JS-beginner/app.js
@@ -25,9 +25,67 @@
 // console.log(c);
 
 // 4 Типы данных
-const isProgrammer = true;
-const firstName = "Max";
-const age = 22;
-console.log(typeof isProgrammer);
-console.log(typeof firstName);
-console.log(typeof age);
+// const isProgrammer = true;
+// const firstName = "Max";
+// const age = 22;
+// console.log(typeof isProgrammer);
+// console.log(typeof firstName);
+// console.log(typeof age);
+// console.log(typeof x);
+// console.log(null);
+
+// 5 Приоритет операторов
+// const fullAge = 22;
+// const birthYear = 2000;
+// const currentYear = 2023;
+
+// const isFullAge = currentYear - birthYear >= fullAge;
+// console.log(isFullAge);
+
+// 6 Условные операторы
+// const courseStatus = "fail"; // ready, fail, pending
+// if (courseStatus === "ready") {
+//   console.log("курс готов, можно проходить");
+// } else if (courseStatus === "pending") {
+//   console.log("курс в процессе разработки");
+// } else console.log("Курс не получился");
+
+// const isReady = true;
+// if (isReady) {
+//   console.log("Всё готово");
+// } else {
+//   console.log("Всё не готово");
+// }
+// =
+// Тернартое выражение
+// isReady ? console.log("Всё готово") : console.log("Всё не готово");
+
+// 7 Булевая логика
+
+// 8 Функции
+// function calculateAge(year) {
+//   return 2023 - year;
+// }
+// // console.log(calculateAge(2000));
+// function logInfoAbout(name, year) {
+//   const age = calculateAge(year);
+
+//   if (age > 0) {
+//     console.log("Человек по имени " + name + " сейчас имеет возраст " + age);
+//   } else {
+//     console.log("Вообще-то это уже будущее");
+//   }
+// }
+// logInfoAbout("Максим", 2000);
+// logInfoAbout("Антон", 2024);
+
+// 9 Массивы
+// const cars = ["Honda", "BMW", "Ford"];
+// console.log(cars);
+// console.log(cars[1]);
+// console.log(cars.length);
+
+// cars[0] = "Porsche";
+// cars[3] = "Mazda";
+// cars[cars.length] = "MAZ";
+// console.log(cars);
