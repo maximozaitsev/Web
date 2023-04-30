93c12840b0011a7cca7f4a67abc75bc8d9607ea0
maximozaitsev <maximozaitsev@gmail.com>
2023-04-30 21:12:17 +0300

    js-async

---
+++ 2023/JS-beginner/07_async.js
@@ -0,0 +1,43 @@
+// Event loop
+
+// const timeout = setTimeout(() => {
+//   console.log("After timeout");
+// }, 2500);
+
+// const interval = setInterval(() => {
+//   console.log("After interval");
+// }, 1000);
+
+// clearInterval(interval);
+
+const delay = (wait = 1000) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, wait);
+  });
+  return promise;
+};
+
+// delay(2500)
+//   .then(() => {
+//     console.log("After 2 seconds");
+//   })
+//   .catch((err) => console.error(err))
+//   .finally(() => console.log("Finally"));
+
+const getData = () => new Promise((resolve) => resolve([1, 1, 2, 3, 5, 8, 13]));
+
+async function asyncExample() {
+  try {
+    await delay(3000);
+    const data = await getData();
+    console.log("Data", data);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    console.log("Finally");
+  }
+}
+
+asyncExample();
